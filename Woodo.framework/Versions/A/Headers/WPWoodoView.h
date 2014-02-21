6225a7b4f3a61ea4025610e771da39d19a308dfd
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2014-02-21 14:28:36 +0200

    Major update

--- Woodo.framework/Versions/A/Headers/WPWoodoView.h
+++ Woodo.framework/Versions/A/Headers/WPWoodoView.h
@@ -6,16 +6,32 @@
 //  Copyright (c) 2013 Woodo. All rights reserved.
 //
 
-@interface WPWoodoView : UIView
 
-#define WoodoPlayerPerformedFinishNotification @"videoplayer.performed.finish"
-#define WoodoPlayerErrorNotification @"videoplayer.error"
+#import <CoreLocation/CoreLocation.h>
+
+@interface WPWoodoView : UIView <CLLocationManagerDelegate>
+
+@property (atomic, readwrite) NSError *error;
+
+@property (nonatomic, readwrite, copy) NSArray *shareRecipients;    // Mail only
+@property (nonatomic, readwrite, copy) NSString *shareTitle;    // Mail only
+@property (nonatomic, readwrite, copy) NSString *shareText;
+//@property (nonatomic, readwrite, copy) NSArray *shareUrls;
+//@property (nonatomic, readwrite, copy) NSArray *shareImages;
+@property (nonatomic, readonly, getter = isPlaying) BOOL playing;
+
+@property (nonatomic, readwrite, strong) void(^startHandler)();
+@property (nonatomic, readwrite, strong) void(^progressUpdateHandler)(CGFloat currentTime, CGFloat duration);    // Works only with user content
+@property (nonatomic, readwrite, strong) void(^finishHandler)();
+@property (nonatomic, readwrite, strong) void(^errorHandler)();
 
 - (void) play:(NSURL *) url
 withAttachment:(UIView *) attachmentView
 withAdvertisementToken:(NSString *) token;
-- (void) pause:(void(^)(BOOL succeed)) resultHandler;
-- (void) resume:(void(^)(BOOL succeed)) resultHandler;
-- (void) seekTo:(CGFloat) percentage resultHandler:(void(^)(BOOL succeed)) resultHandler;
+- (void) pause;
+- (void) resume;
+- (void) seekTo:(CGFloat) percentage;
+
+- (void) toggleFullscreen;
 
 @end
