d46584d37cc20c97aeaa82ae2393e174ee675dfe
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2014-02-25 15:26:03 +0200

    Updated framework version to v1.0.1

--- Woodo.framework/Versions/A/Headers/WPWoodoView.h
+++ Woodo.framework/Versions/A/Headers/WPWoodoView.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Woodo. All rights reserved.
 //
 
-
+#import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
 @interface WPWoodoView : UIView <CLLocationManagerDelegate>
